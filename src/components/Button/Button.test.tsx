3ce5e604a901074cc1fadae2fecd30b100d59235
Tomasz <skretkowskitomasz@gmail.com>
2022-09-05 17:17:15 +0200

    fix: test code refactored

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 
-import { Button, primaryBtn, secondaryBtn } from './Button';
+import { Button } from './Button';
 
 describe('Button tests', () => {
   test('if primary button is rendered', () => {
-    render(<Button variant={primaryBtn} text="primary" />);
+    render(<Button variant="primary" label="Primary" />);
     expect(screen.getByText(/primary/i)).toBeInTheDocument();
   });
   test('if secondary button is rendered', () => {
-    render(<Button variant={secondaryBtn} text="secondary" />);
+    render(<Button variant="secondary" label="Secondary" />);
     expect(screen.getByText(/secondary/i)).toBeInTheDocument();
   });
 });
