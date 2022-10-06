4dfc17e938f2fb2c92038b0f53c3f5e09a3135c6
Tomasz <skretkowskitomasz@gmail.com>
2022-10-06 16:13:07 +0200

    fix: mock icon eslint disable

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,4 +1,5 @@
-export const Icon = ({ color }: any) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Icon = (color: any) => {
   return (
     <div className={`h-6 w-6 ${color}`} data-testid="icon">
       Ico
