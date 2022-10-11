283a9aee7eb52d49eb8f8854a7e371f4981b66b1
Tomasz <skretkowskitomasz@gmail.com>
2022-10-11 14:11:25 +0200

    fix: test card component props refactored

--- src/components/Card/card.test.tsx
+++ src/components/Card/card.test.tsx
@@ -7,10 +7,13 @@ describe('Card tests', () => {
   test('if card is rendered', () => {
     render(
       <Card
-        header="Kim Jesteśmy ?"
-        title="Organizacja non-profit 
-    z Wrocławia"
-        text="Najważniejszą rzeczą dla nas jest rozwój – tworzenie społeczności, w której osoby gotowe do działania mogą doskonalić swoje umiejętności."
+        articles={[
+          {
+            header: 'Kim Jesteśmy ?',
+            title: 'Organizacja non-profit z Wrocławia',
+            text: 'Najważniejszą rzeczą dla nas jest rozwój – tworzenie społeczności, w której osoby gotowe do działania mogą doskonalić swoje umiejętności.',
+          },
+        ]}
       />,
     );
     const card = screen.getByTestId('card');
