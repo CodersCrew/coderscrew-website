7dba1decc535d49947202608eaa44f6dcdc88166
Tomasz <skretkowskitomasz@gmail.com>
2022-10-12 15:20:05 +0200

    refactor: destructure article parameters

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -11,11 +11,11 @@ export type CardProps = {
 export const Card = ({ articles }: CardProps) => {
   return (
     <div className="rounded-lg shadow-card" data-testid="card">
-      {articles.map((article) => (
-        <article className="flex h-max w-full flex-col gap-3 p-5" key={article.title}>
-          {article.header ? <p className="text-sm uppercase tracking-wider text-blue">{article.header}</p> : null}
-          <h2 className="text-title font-medium tracking-wide text-primary">{article.title}</h2>
-          <p className="text-base leading-6 text-secondary">{article.text}</p>
+      {articles.map(({ title, header, text }) => (
+        <article className="flex h-max w-full flex-col gap-3 p-5" key={title}>
+          {header ? <p className="text-sm uppercase tracking-wider text-blue">{header}</p> : null}
+          <h2 className="text-title font-medium tracking-wide text-primary">{title}</h2>
+          <p className="text-base leading-6 text-secondary">{text}</p>
         </article>
       ))}
     </div>
