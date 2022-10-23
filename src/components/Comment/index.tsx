0631424dc652257959db6593a7a0caa50c6107ed
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2022-10-23 14:04:42 +0200

    fix: ignore .yarn/ and fix test case

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -5,7 +5,7 @@ import { Hexagon } from '../Hexagon';
 export type CommentProps = { text: string; name: string };
 
 export const Comment = ({ text, name }: CommentProps) => (
-  <div className="bg-white flex h-full w-full flex-col items-center gap-5 rounded py-8 shadow-md" data-testId="comment">
+  <div className="bg-white flex h-full w-full flex-col items-center gap-5 rounded py-8 shadow-md" data-testid="comment">
     <div className="h-13 w-13">
       <Hexagon variant="default" primaryColor="blue" secondaryColor="blue" icon opacity={10} />
     </div>
