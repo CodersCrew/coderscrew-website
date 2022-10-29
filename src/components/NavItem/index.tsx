664e7eeef5a6efb383bb96c5c41f9fd9cdd7225b
Tomasz <skretkowskitomasz@gmail.com>
2022-10-29 15:05:17 +0200

    refactor: clean code

--- src/components/NavItem/index.tsx
+++ src/components/NavItem/index.tsx
@@ -19,44 +19,43 @@ export type DropdownItemsProps = {
 };
 
 export const NavItem = ({ item: { dropdownItems, label } }: NavItemProps) => {
-  const [open, setOpen] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
-  const openButtonClasses = open ? 'text-quaternary inline' : 'text-primary';
+  const buttonClasses = showDropdown ? 'text-quaternary inline' : 'text-primary';
   const dropdownItemClasses =
     'hover:text-quaternary hover:bg-additional-darkWhite px-5 leading-5 py-3 last:rounded-b-[4px]';
 
-  const handleClick = () => {
-    setOpen((prev) => !prev);
-  };
+  const handleShowDropdown = () => setShowDropdown((prev) => !prev);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-3">
       <button
-        className={`w-full cursor-pointer text-sm font-bold ${openButtonClasses}`}
+        className={`w-full cursor-pointer text-sm font-bold ${buttonClasses}`}
         data-testid="navItem"
-        onClick={handleClick}
-        onBlur={handleClick}
+        onClick={handleShowDropdown}
       >
         {label}{' '}
-        <span className={`pl-1 ${openButtonClasses}`}>
-          {open ? (
+        <span className={`pl-1 ${buttonClasses}`}>
+          {showDropdown ? (
             <Image width={12} height={12} src={arrowUp} alt="arrowUp" />
           ) : (
             <Image width={12} height={12} src={arrow} alt="arrow" />
           )}
         </span>
       </button>
-      <div
-        className={`flex w-auto cursor-pointer flex-col rounded-[4px]  text-left text-base  font-normal text-primary shadow-card  ${
-          open ? 'visible' : 'hidden'
+      <ul
+        className={`flex w-auto cursor-pointer flex-col rounded-[4px] text-left text-base font-normal text-primary shadow-card ${
+          showDropdown ? 'visible' : 'hidden'
         }`}
       >
         {dropdownItems?.map(({ dropdownItemLabel, path }) => (
-          <Link href={path} key={dropdownItemLabel}>
-            <p className={dropdownItemClasses}>{dropdownItemLabel}</p>
-          </Link>
+          <li key={dropdownItemLabel} className={dropdownItemClasses}>
+            <Link href={path} onClick={handleShowDropdown}>
+              {dropdownItemLabel}
+            </Link>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
