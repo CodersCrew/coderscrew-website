510939c5dd6eb375df1c1211d4fd10a4b389d0a0
Marcin <marcinkukulka@gmail.com>
2022-11-15 11:05:21 +0100

    refactor: separate field into smaller components

--- src/containers/WhoWeAre/Field.tsx
+++ src/containers/WhoWeAre/Field.tsx
@@ -2,17 +2,42 @@ import Image, { StaticImageData } from 'next/image';
 
 import { Button } from '../../components/Button/index';
 
-type FieldProps = {
+type ImageContainerProps = {
   imageSrc: string | StaticImageData;
   imageAlt: string;
-  className: string;
-  contentClassName: string;
   imageClassName: string;
+};
+
+type ContenContainerProps = {
+  contentClassName: string;
   buttonLabel: string;
   questionField: string;
   title: string;
   content: string;
 };
+
+interface FieldProps extends ImageContainerProps, ContenContainerProps {
+  className: string;
+}
+
+const ImageContainer = ({ imageClassName, imageSrc, imageAlt }: ImageContainerProps) => (
+  <div className={`flex h-full w-full ${imageClassName}`}>
+    <Image width={558} height={316} src={imageSrc} alt={imageAlt} objectFit="contain" />
+  </div>
+);
+
+const ContentContainer = ({ contentClassName, questionField, title, content, buttonLabel }: ContenContainerProps) => (
+  <div
+    className={`mt-10 flex max-h-[316px] max-w-[558px] flex-col space-y-3 pr-13 lg:mt-0 lg:pr-6 xl:space-y-4 xl:pr-16 ${contentClassName}`}
+  >
+    <p className="text-sm font-semibold uppercase text-quaternary">{questionField}</p>
+    <p className="text-[2rem] font-semibold">{title}</p>
+    <p>{content}</p>
+    <div className="pt-2">
+      <Button label={buttonLabel} onClick={() => {}} type="button" variant="primary" />
+    </div>
+  </div>
+);
 
 export const Field = ({
   imageSrc,
@@ -26,18 +51,14 @@ export const Field = ({
   buttonLabel
 }: FieldProps) => (
   <div className={`mx-auto flex flex-col justify-center lg:mx-0 lg:flex-row ${className}`}>
-    <div className={`flex h-full w-full ${imageClassName}`}>
-      <Image width={558} height={316} src={imageSrc} alt={imageAlt} objectFit="contain" />
-    </div>
-    <div
-      className={`mt-10 flex max-h-[316px] max-w-[558px] flex-col space-y-3 pr-13 lg:mt-0 lg:pr-6 xl:space-y-4 xl:pr-16 ${contentClassName}`}
-    >
-      <p className="text-sm font-semibold uppercase text-quaternary">{questionField}</p>
-      <p className="text-[2rem] font-semibold">{title}</p>
-      <p>{content}</p>
-      <div className="pt-2">
-        <Button label={buttonLabel} onClick={() => {}} type="button" variant="primary" />
-      </div>
-    </div>
+    <ImageContainer imageClassName={imageClassName} imageSrc={imageSrc} imageAlt={imageAlt} />
+
+    <ContentContainer
+      contentClassName={contentClassName}
+      questionField={questionField}
+      title={title}
+      content={content}
+      buttonLabel={buttonLabel}
+    />
   </div>
 );
