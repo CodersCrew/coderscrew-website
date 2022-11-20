fe2eb928174a8477473010d5b2c5150bdc91d579
Marcin <marcinkukulka@gmail.com>
2022-11-20 22:27:17 +0100

    fix: refactoring stories

--- src/containers/AboveTheFold/aboveTheFold.stories.tsx
+++ src/containers/AboveTheFold/aboveTheFold.stories.tsx
@@ -4,17 +4,20 @@ import { aboveTheFoldData } from '../../common/testData';
 import { AboveTheFold, AboveTheFoldProps } from '.';
 
 export default {
-title: 'Above the fold',
-component: AboveTheFold
+  title: 'Above the fold',
+  component: AboveTheFold
 };
 
 const Template: Story<AboveTheFoldProps> = (args: AboveTheFoldProps) => <AboveTheFold {...args} />;
 
 export const Default = Template.bind({});
+
+const { imageSrc, imageAlt, title, content, buttonContent } = aboveTheFoldData;
+
 Default.args = {
-  imageSrc: aboveTheFoldData.imageSrc,
-  imageAlt: aboveTheFoldData.imageAlt,
-  title: aboveTheFoldData.title,
-  content: aboveTheFoldData.content,
-  buttonContent: aboveTheFoldData.buttonContent
+  imageSrc,
+  imageAlt,
+  title,
+  content,
+  buttonContent
 };
