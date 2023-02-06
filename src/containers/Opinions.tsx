d79776d629407db2c583081938f69a960766cf1a
Marcin <marcinkukulka@gmail.com>
2023-02-06 23:59:47 +0100

    feat: created carousle

--- src/containers/Opinions.tsx
+++ src/containers/Opinions.tsx
@@ -1,7 +1,41 @@
 import { Link } from '@/components';
 
+const carouselData = [
+  {
+    content:
+      '“ Dzięki CodersCrew mogę poszerzać moje doświadczenie w zarządzaniu projektami IT. Organizacja dała mi możliwość działania na prawdziwych projektach. Poznałam też mnóstwo cudownie kreatywnych ludzi, których zapał do działania nieustannie podziwiam! ”',
+    author: 'Kornelia',
+    id: 1
+  },
+  {
+    content:
+      '“ Dzięki CodersCrew mogę poszerzać moje doświadczenie w zarządzaniu projektami IT. Organizacja dała mi możliwość działania na prawdziwych projektach. Poznałam też mnóstwo cudownie kreatywnych ludzi, których zapał do działania nieustannie podziwiam! ”',
+    author: 'Kornelia',
+    id: 2
+  },
+  {
+    content:
+      '“ Dzięki CodersCrew mogę poszerzać moje doświadczenie w zarządzaniu projektami IT. Organizacja dała mi możliwość działania na prawdziwych projektach. Poznałam też mnóstwo cudownie kreatywnych ludzi, których zapał do działania nieustannie podziwiam! ”',
+    author: 'Kornelia',
+    id: 3
+  }
+];
+
+type CarouselSlideProps = {
+  content: string;
+  author: string;
+};
+
+const CarouselSlide = ({ content, author }: CarouselSlideProps) => (
+  <div className="flex w-[290px] flex-col items-center rounded-md bg-additional-white p-6 shadow-card lg:w-[510px]">
+    <div className="mb-6 h-[56px] w-[56px] bg-primary" />
+    <div className="mb-4 text-center text-base font-semibold leading-normal">{content}</div>
+    <div className="text-center text-base text-secondary">– {author}</div>
+  </div>
+);
+
 export const Opinions = () => (
-  <div className="relative z-10 flex flex-col items-center py-24">
+  <div className="relative z-10 flex flex-col items-center overflow-x-hidden py-24">
     <div className="mb-12 max-w-sm lg:max-w-2xl">
       <h2 className="mb-4 text-center text-2xl font-bold uppercase text-quaternary">Mówią o nas</h2>
       <p className="text-center text-xl leading-normal text-additional-white">
@@ -9,16 +43,10 @@ export const Opinions = () => (
         Cię to, co robimy.
       </p>
     </div>
-    <div className="mb-18">
-      <div className="flex lg:w-[510px] w-[290px] flex-col items-center bg-additional-white p-6 shadow-card">
-        <div className="mb-6 h-[56px] w-[56px] bg-primary" />
-        <div className="mb-4 text-center text-base font-semibold leading-normal">
-          “ Dzięki CodersCrew mogę poszerzać moje doświadczenie w zarządzaniu projektami IT. Organizacja dała mi
-          możliwość działania na prawdziwych projektach. Poznałam też mnóstwo cudownie kreatywnych ludzi, których zapał
-          do działania nieustannie podziwiam! ”
-        </div>
-        <div className="text-center text-base text-secondary">– Kornelia</div>
-      </div>
+    <div className="mb-18 flex items-center gap-8 pl-8">
+      {carouselData.map((slide) => (
+        <CarouselSlide content={slide.content} author={slide.author} key={slide.id} />
+      ))}
       <div />
     </div>
     <div className="flex gap-4">
