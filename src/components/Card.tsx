3b64f53e9f3cd0febc55fde6451e9b1353668dd1
Marcin Kukułka <marcinkukulka@gmail.com>
2023-02-09 17:49:16 +0100

    [84] rwd (#89)
    Co-authored-by: jedrzejratajczak <nilphym@gmail.com>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export const Card = ({ header, title, text }: CardProps) => (
       ${header ? 'gap-5' : 'gap-2'} rounded-md bg-additional-white p-8 shadow-card`}
   >
     {header && <h4 className="text-sm font-semibold uppercase tracking-wider text-quaternary">{header}</h4>}
-    <h2 className="text-3.5xl font-semibold leading-tight text-primary">{title}</h2>
+    <h2 className="text-2xl font-semibold leading-tight text-primary lg:text-3.5xl">{title}</h2>
     <p className="text-base leading-normal text-primary">{text}</p>
   </div>
 );
--- src/containers/Opinions.tsx
+++ src/containers/Opinions.tsx
@@ -1,8 +1,8 @@
 import { Link } from '@/components';
 
 export const Opinions = () => (
-  <div className="relative z-10 flex flex-col items-center py-24">
-    <div className="mb-12 max-w-2xl">
+  <div className="relative z-10 flex flex-col items-center px-4 py-24">
+    <div className="mb-12 max-w-sm lg:max-w-2xl">
       <h2 className="mb-4 text-center text-2xl font-bold uppercase text-quaternary">Mówią o nas</h2>
       <p className="text-center text-xl leading-normal text-additional-white">
         Dołącz do ponad 100 członków CodersCrew, którzy rozwijają swoje pasje. Zostań naszym partnerem, jeśli interesuje
@@ -10,7 +10,7 @@ export const Opinions = () => (
       </p>
     </div>
     <div className="mb-18">
-      <div className="flex w-[510px] flex-col items-center bg-additional-white p-6 shadow-card">
+      <div className="flex w-[290px] flex-col items-center bg-additional-white p-6 shadow-card md:w-[510px]">
         <div className="mb-6 h-[56px] w-[56px] bg-primary" />
         <div className="mb-4 text-center text-base font-semibold leading-normal">
           “ Dzięki CodersCrew mogę poszerzać moje doświadczenie w zarządzaniu projektami IT. Organizacja dała mi
@@ -21,7 +21,7 @@ export const Opinions = () => (
       </div>
       <div />
     </div>
-    <div className="flex gap-4">
+    <div className="flex flex-col items-center gap-4 md:flex-row md:items-stretch">
       <Link href="#" variant="outlined">
         Zostań partnerem
       </Link>
@@ -29,6 +29,6 @@ export const Opinions = () => (
         Dołącz do nas
       </Link>
     </div>
-    <div className="absolute top-0 left-0 right-0 bottom-1/3 -z-10 bg-primary" />
+    <div className="absolute top-0 left-0 right-0 bottom-[43%] -z-10 bg-primary" />
   </div>
 );
