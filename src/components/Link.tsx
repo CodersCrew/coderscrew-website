e33aed798889279ff78d4b984d65fd6583b4d363
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-03-25 23:08:24 +0100

    Fix link style

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,10 +2,10 @@ import { default as NextLink } from 'next/link';
 
 const variants = {
   outlined:
-    'border-2 border-quaternary text-quaternary hover:border-guild-webDev hover:bg-guild-webDev hover:text-additional-white hover:shadow-button',
+    'font-bold border-2 border-quaternary text-quaternary hover:border-guild-webDev hover:bg-guild-webDev hover:text-additional-white hover:shadow-button',
   filled:
-    'border-2 border-quaternary bg-quaternary text-additional-white hover:border-guild-webDev hover:bg-guild-webDev hover:shadow-button',
-  bare: 'hover:underline'
+    'font-bold border-2 border-quaternary bg-quaternary text-additional-white hover:border-guild-webDev hover:bg-guild-webDev hover:shadow-button',
+  bare: 'font-semibold hover:underline'
 };
 
 interface LinkProps {
@@ -18,7 +18,7 @@ interface LinkProps {
 export const Link = ({ children, href, variant, className, ...props }: LinkProps) => (
   <NextLink href={href} passHref>
     <a
-      className={`flex w-max items-center rounded-lg py-3 px-6 text-base font-bold leading-tight ${variants[variant]} ${className}`}
+      className={`flex w-max items-center rounded-lg py-3 px-6 text-base leading-tight ${variants[variant]} ${className}`}
       {...props}
     >
       {children}
