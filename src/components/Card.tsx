4225d5ff0aee06a395f09145e4e7a13c273ee744
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-03-26 00:10:35 +0100

    Refactor card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,13 @@
-interface CardProps {
-  header?: string;
+type CardProps = {
+  header: string;
   title: string;
   text: string;
-}
+};
 
 export const Card = ({ header, title, text }: CardProps) => (
-  <div
-    className={`flex w-full max-w-sm flex-col xs:w-[390px]
-      ${header ? 'gap-5' : 'gap-2'} rounded-md bg-additional-white p-8 shadow-card`}
-  >
-    {header && <h4 className="text-sm font-semibold uppercase tracking-wider text-quaternary">{header}</h4>}
-    <h2 className="text-3.5xl font-semibold leading-tight text-primary">{title}</h2>
+  <div className="flex max-w-sm flex-col gap-4 rounded-md bg-white p-6 shadow-card lg:gap-5 xs:w-max xs:p-8">
+    <p className="text-sm font-semibold uppercase tracking-wider text-quaternary">{header}</p>
+    <p className="text-2xl font-semibold leading-tight text-primary lg:text-3.5xl">{title}</p>
     <p className="text-base leading-normal text-primary">{text}</p>
   </div>
 );
