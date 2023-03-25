e08de3549456d48df4d3b5f5f0a39914edc5958f
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-03-26 00:10:22 +0100

    Refactor projects

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -4,23 +4,24 @@ import Untitled1 from '@/common/assets/Untitled1.png';
 import Untitled2 from '@/common/assets/Untitled2.png';
 import Untitled3 from '@/common/assets/Untitled3.png';
 import { Link } from '@/components';
+import { SectionWrapper } from '@/components/PageWrapper';
 
 export const Projects = () => (
-  <div className="mx-auto flex max-w-screen-2xl flex-col items-center gap-15 px-5 py-24 lg:gap-30 lg:px-30">
-    <div className="max-w-lg xl:max-w-2xl">
-      <h2 className="mb-4 text-center text-2xl font-bold uppercase text-quaternary">Nasze projekty</h2>
+  <SectionWrapper className="flex flex-col items-center gap-12 lg:gap-30">
+    <div className="flex flex-col gap-4">
+      <p className="text-center text-2xl font-semibold uppercase text-quaternary">Nasze projekty</p>
       <p className="text-center text-xl leading-normal">
         nasze projekty.nasze projekty.nasze projekty.nasze projekty.nasze projekty.nasze projekty.nasze projekty.nasze
         projekty.
       </p>
     </div>
-    <div className="flex flex-col gap-14 lg:flex-row">
-      <Image src={Untitled2} alt="Picture of the author" width={430} height={280} objectFit="cover" />
-      <Image src={Untitled1} alt="Picture of the author" width={430} height={280} objectFit="cover" />
-      <Image src={Untitled3} alt="Picture of the author" width={430} height={280} objectFit="cover" />
+    <div className="flex flex-col gap-12 lg:flex-row">
+      <Image src={Untitled2} alt="Project 1" width={430} height={280} objectFit="cover" />
+      <Image src={Untitled1} alt="Project 2" width={430} height={280} objectFit="cover" />
+      <Image src={Untitled3} alt="Project 3" width={430} height={280} objectFit="cover" />
     </div>
     <Link href="#" variant="filled">
       Wszystkie projekty
     </Link>
-  </div>
+  </SectionWrapper>
 );
