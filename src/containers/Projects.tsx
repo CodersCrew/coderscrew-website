48086b189431c51153b86b48685d10a27fe497f9
Igor Romański <33060719+twomeaningsof@users.noreply.github.com>
2023-05-07 13:17:03 +0200

    [97] Adds ImageOnHover component (projects container) (#98)

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image';
-
 import Untitled1 from '@/common/assets/Untitled1.png';
 import Untitled2 from '@/common/assets/Untitled2.png';
 import Untitled3 from '@/common/assets/Untitled3.png';
 import { Link, SectionWrapper } from '@/components';
+import { ImageWithHover } from '@/components/ImageWithHover';
 
 export const Projects = () => (
   <SectionWrapper className="flex flex-col items-center gap-12 lg:gap-30">
@@ -15,9 +14,36 @@ export const Projects = () => (
       </p>
     </div>
     <div className="flex flex-col gap-12 lg:flex-row">
-      <Image src={Untitled2} alt="Project 1" width={430} height={280} objectFit="cover" />
-      <Image src={Untitled1} alt="Project 2" width={430} height={280} objectFit="cover" />
-      <Image src={Untitled3} alt="Project 3" width={430} height={280} objectFit="cover" />
+      <ImageWithHover
+        src={Untitled2}
+        alt="Project 1"
+        width={430}
+        height={280}
+        objectFit="cover"
+        title="Coders Camp"
+        text="Największy otwarty kurs programowania webowego"
+        link="#"
+      />
+      <ImageWithHover
+        src={Untitled1}
+        alt="Project 2"
+        width={430}
+        height={280}
+        objectFit="cover"
+        title="Przejście centrum innowacji"
+        text="Warsztaty z podstaw programowania webowego"
+        link="#"
+      />
+      <ImageWithHover
+        src={Untitled3}
+        alt="Project 3"
+        width={430}
+        height={280}
+        objectFit="cover"
+        title="Jira Sticky Notes"
+        text="Add-on do największego narzędzia do zarządzania projektami IT"
+        link="#"
+      />
     </div>
     <Link href="#" variant="filled">
       Wszystkie projekty
