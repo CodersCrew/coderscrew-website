6ba57585643a8bf544f8e7511a4a31580139a75b
michal-karalus <116072067+michal-karalus@users.noreply.github.com>
2023-05-17 23:03:57 +0200

    [100] Add favicon (#101)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '@/common/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <link rel="icon" type="image/png" href="/coders-crew.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
