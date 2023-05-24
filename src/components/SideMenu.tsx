9b91770f9bb5635dbbde11983654fd7ad86b2271
kashubian <karalusmichal@gmail.com>
2023-05-24 22:29:27 +0200

    refactor: side menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,26 +1,33 @@
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import BurgerMenu from '@/common/assets/burgerMenu.svg';
 import CrossMenu from '@/common/assets/crossMenu.svg';
 
 import { Link } from './Link';
 
-type SideMenuProps = {
-  isOpen: boolean;
-  onClose: () => void;
-};
+export const SideMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export const SideMenu = ({ isOpen, onClose }: SideMenuProps) => {
-  const handleClick = () => {
-    onClose();
+  const toggleMenu = () => {
+    setIsOpen((current) => !current);
   };
 
   return (
     <>
-      <div className={`fixed inset-0  opacity-20 lg:hidden ${isOpen ? 'bg-primary' : ''}`} />
+      <BurgerMenu className="z-50 h-6 sm:h-8 lg:hidden" onClick={toggleMenu} />
       <div
-        className={`fixed inset-y-0 left-0 w-70 bg-primary text-tetriary transition-all ${
+        className={twMerge(
+          'fixed inset-0 -translate-x-full bg-primary opacity-20 transition-all duration-[50ms]',
+          isOpen && 'translate-x-0 '
+        )}
+      />
+      <div
+        className={`fixed inset-y-0 left-0 w-70 bg-primary text-tetriary transition-all lg:hidden ${
           !isOpen ? '-translate-x-70' : ''
         }`}
       >
-        <button onClick={handleClick} className="m-6 ml-auto flex">
+        <button onClick={toggleMenu} className="m-6 ml-auto flex">
           <CrossMenu />
         </button>
         <nav className="flex-grow">
@@ -32,10 +39,10 @@ export const SideMenu = ({ isOpen, onClose }: SideMenuProps) => {
           </Link>
           <Link className="block py-4 px-5" variant="bare" href="/partnerzy">
             Partnerzy
-          </Link>{' '}
+          </Link>
           <Link className="block py-4 px-5" variant="bare" href="/wspolpraca">
             Współpraca
-          </Link>{' '}
+          </Link>
           <Link className="block py-4 px-5" variant="bare" href="/rekrutacja">
             Dołącz do nas
           </Link>
