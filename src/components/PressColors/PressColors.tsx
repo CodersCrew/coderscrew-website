6d3a70644dadd5e10b547a3f759c195d41117dcf
Damian <damian.gatkowski@proton.me>
2023-06-21 11:22:16 +0200

    feat: center PressCard PressColors

--- src/components/PressColors/PressColors.tsx
+++ src/components/PressColors/PressColors.tsx
@@ -11,14 +11,16 @@ export const PressColors: React.FC<PressColorsProps> = ({
   hex,
   rgb
 }) => (
-  <div className="h-48 rounded-8 w-56 flex flex-col items-center gap-6 bg-white p-0 pb-8">
-    <div className={`h-20 rounded-lg	p-4 shadow-lg ${div}`}></div>
-    <p className="mb-2 text-xl font-semibold">{title}</p>
-    <p className="text-transform: uppercase; text-base leading-normal text-primary">
-      {hex}
-    </p>
-    <p className="text-transform: uppercase; text-base leading-normal text-primary">
-      {rgb}
-    </p>
+  <div className="h-48 flex w-full flex-col bg-white p-0 pb-8">
+    <div className={`w-80 h-20 rounded-lg p-4 shadow-lg ${div}`}></div>
+    <div className="h-48 flex w-full flex-col items-center bg-white p-0 pb-8">
+      <p className="mb-2 p-4 text-xl font-semibold">{title}</p>
+      <p className="text-transform: uppercase; text-base leading-normal text-primary">
+        {hex}
+      </p>
+      <p className="text-transform: uppercase; text-base leading-normal text-primary">
+        {rgb}
+      </p>
+    </div>
   </div>
 );
--- src/pages/do-pobrania/press-pack.tsx
+++ src/pages/do-pobrania/press-pack.tsx
@@ -7,65 +7,78 @@ import { PressCard } from '@/components/PressCard';
 import { PressColors } from '@/components/PressColors';
 
 const PressPack = () => (
-  <SectionWrapper className="grid items-center justify-center gap-18 overflow-hidden lg:grid-cols-[1fr,min-content]">
+  <SectionWrapper className="grid items-center gap-18 overflow-hidden lg:grid-cols-[1fr,min-content]">
     <div className="flex flex-col gap-y-10">
-      <div className="flex flex-col gap-4">
+      <div className="items-left flex flex-col gap-4">
         <h1 className="mb-8 text-3.5xl font-bold leading-tight md:mb-25 md:text-5xl">
-          PressPack{' '}
+          PressPack
         </h1>
-        <p className="text-center text-xl leading-normal">
+        <p className="items-left text-xl leading-normal">
           Tutaj możesz pobrać nasz logotyp w różnych wersjach, w zależności od
           potrzeb. Dołożyliśmy również oficjalne kolory wykorzystywane w
           komunikacji wizualnej naszego stowarzyszenia.
         </p>
       </div>
-      <div className="w-360 h-263 rounded-8 flex flex-col items-center justify-center gap-4 bg-white p-0 pb-8 md:flex-row">
-        <PressCard
-          src={Frame641}
-          alt="Logo ciemne"
-          title="Logo ciemne"
-          link="#"
-        />
-        <PressCard
-          src={Frame641black}
-          alt="Logo białe"
-          title="Logo białe"
-          link="#"
-        />
-      </div>
-      <div className="w-360 h-263 rounded-8 flex flex-col items-center justify-center gap-4 bg-white p-0 pb-8 md:flex-row">
-        <PressCard
-          src={Frame761}
-          alt="Logo czarne druk"
-          title="Logo czarne druk"
-          link="#"
-        />
-        <PressCard
-          src={Frame761black}
-          alt="Logo białe druk"
-          title="Logo białe druk"
-          link="#"
-        />
-      </div>
-      <div className="w-56 flex flex-col gap-12 lg:flex-row">
-        <PressColors
-          div="bg-white"
-          title="Biały"
-          hex="hex: #ffffff"
-          rgb="rgb: 255, 255, 255"
-        />
-        <PressColors
-          div="bg-quaternary"
-          title="Niebieski"
-          hex="hex: #1a90ff"
-          rgb="rgb: 26, 144, 255"
-        />
-        <PressColors
-          div="bg-black"
-          title="Czarny"
-          hex="hex: #292929"
-          rgb="rgb: 41, 41, 41"
-        />
+      <div
+        className="align -center flex flex-col
+      items-center"
+      >
+        <div className="items-left flex max-w-3xl flex-col justify-center">
+          <h2 className="mb-4 items-center text-2xl font-bold">Logo</h2>
+          <div className="w-360 rounded-8 flex flex-col items-center justify-center gap-2 bg-white p-0 pb-8 md:flex-row">
+            <PressCard
+              src={Frame641}
+              alt="Logo ciemne"
+              title="Logo ciemne"
+              link="#"
+            />
+            <PressCard
+              src={Frame641black}
+              alt="Logo białe"
+              title="Logo białe"
+              link="#"
+            />
+          </div>
+          <div className="w-360 rounded-8 flex flex-col items-center justify-center gap-2 bg-white p-0 pb-8 md:flex-row">
+            <PressCard
+              src={Frame761}
+              alt="Logo czarne druk"
+              title="Logo czarne druk"
+              link="#"
+            />
+            <PressCard
+              src={Frame761black}
+              alt="Logo białe druk"
+              title="Logo białe druk"
+              link="#"
+            />
+          </div>
+          <div className="w-56 flex flex-col gap-12">
+            <h2 className="mb-4 items-center text-2xl font-bold">
+              Kolory CoderCrew
+            </h2>
+            <div className="w-56 flex flex-col gap-12 lg:flex-row">
+              <PressColors
+                div="bg-white"
+                title="Biały"
+                hex="hex: #ffffff"
+                rgb="rgb: 255, 255, 255"
+              />
+              <PressColors
+                div="bg-quaternary"
+                title="Niebieski"
+                hex="hex: #1a90ff"
+                rgb="rgb: 26, 144, 255"
+              />
+              <PressColors
+                div="bg-black"
+                title="Czarny"
+                hex="hex: #292929"
+                rgb="rgb: 41, 41, 41"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </SectionWrapper>
