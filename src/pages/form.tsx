b24c3ce8944887ba9d4f06b35b50aa44ba54287e
Damian <damian.gatkowski@proton.me>
2023-06-28 13:19:27 +0200

    feat: add section with img and chart

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -9,22 +9,71 @@ import { FormJoin } from '@/components/FormJoin';
 const Formular = () => (
   <SectionWrapper className="flex flex-col">
     <section className="bg-white">
-      <h1>Rozpocznij z nami swoją przygodę w branży IT!</h1>
-      <div>
-        <div>
+      <h1 className="text-3xl font-bold leading-10">
+        Rozpocznij z nami swoją przygodę w branży IT!
+      </h1>
+      <div className="flex flex-row">
+        <div className="flex flex-col">
           <p>
             Będziesz mógł tworzyć zmieniające świat projekty. Mając takie
             portfolio z pewnością zawojujesz rynek pracy!
           </p>
-          <button>Przejdź do formularza</button>
+          <button className="border">Przejdź do formularza</button>
         </div>
         <div>
           <Image src={Zespol} alt="Zespol" className="" />
         </div>
       </div>
+      <div className="flex flex-col">
+        <h2>Dlaczego warto?</h2>
+        <p>Postawisz pierwsze kroki w IT</p>
+        <p>Wykorzystasz swój potencjał</p>
+        <p>
+          Będziesz uczestniczył w ambitnych projektach, które staną się
+          wspaniałym wpisem do Twojego CV
+        </p>
+      </div>
     </section>
     <section className="w-full bg-grey">
       <h1>Nasz proces rekrutacyjny</h1>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col">
+          <img src="" alt="" />
+          <span>1. Wypełnienie formularza</span>
+          <p>
+            Na początek poprosimy Cię o wypełnienie poniższego formularza.
+            Zajmie Ci to maksymalnie 10 minut.
+          </p>
+        </div>
+        <img src="" alt="" />
+        <div className="flex flex-col">
+          <img src="" alt="" />
+          <span>2. Rozpatrzenie zgłoszenia </span>
+          <p>
+            Dzięki niemu lepiej Cię poznamy. Dowiemy się, czy Twoje zdolności
+            odpowiadają naszym aktualnym potrzebom.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col">
+          <img src="" alt="" />
+          <span>3. Rozmowa kwalifikacyjna</span>
+          <p>
+            Porozmawiamy o Twoim doświadczeniu, oczekiwaniach, dyspozycyjności.
+            Odpowiemy także na wszystkie pytania, jakie Ci się nasuną.{' '}
+          </p>
+        </div>
+        <img src="" alt="" />
+        <div className="flex flex-col">
+          <img src="" alt="" />
+          <span>4. Dołączenie do nas</span>
+          <p>
+            Jeśli rozmowa wypadnie pomyślnie, zostaniesz jednym z nas. Od teraz
+            możemy razem zmieniać świat na lepsze!
+          </p>
+        </div>
+      </div>
     </section>
     <div className="pb-10 text-2xl font-bold">
       <h2>Cześć!</h2>
