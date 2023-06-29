7462413fa3ab5ef527c716d15c17da1533e9b5d4
Damian <damian.gatkowski@proton.me>
2023-06-29 11:40:48 +0200

    style: align element in form layout

--- src/components/FormJoin/FormJoin.tsx
+++ src/components/FormJoin/FormJoin.tsx
@@ -1,4 +1,6 @@
 // import React from 'react';
+import Koperta from 'assets/Koperta.svg';
+import Pinezka from 'assets/Pinezka.svg';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 type Inputs = {
@@ -21,113 +23,149 @@ export function FormJoin() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="">
-        <div className="mb-6 flex flex-col">
-          <label htmlFor="name" className="font-normal text-tetriary">
-            Imię i nazwisko
-          </label>
-          <input
-            type="text"
-            className="form-input mb-3 w-3/5 rounded-lg border-hidden bg-formField	px-4 py-3 leading-10 text-tetriary "
-            {...register('name', { required: true })}
-          />
-          {errors.name && (
-            <span className="text-marketing">To pole jest wymagane</span>
-          )}
-        </div>
-        <div className="mb-6 flex flex-col">
-          <label htmlFor="email" className="font-normal text-tetriary">
-            E-mail
-          </label>
-          <input
-            type="email"
-            className="form-input mb-3 w-3/5 rounded-lg border-hidden bg-formField	px-4 py-3 leading-10 text-tetriary "
-            {...register('email', { required: true })}
-          />
-          {errors.email && (
-            <span className="text-marketing">
-              Błędny format e-mail (brak znaków specjalnych np. @, .)
+      <main className="flex w-full flex-col md:flex-row">
+        <section className="flex w-full flex-col">
+          <div className="">
+            <div className="mb-6 flex flex-col">
+              <label
+                htmlFor="name"
+                className="font-normal leading-10 text-tetriary"
+              >
+                Imię i nazwisko
+              </label>
+              <input
+                type="text"
+                className="form-input mb-3 w-3/5 rounded-2xl border-hidden bg-formField	px-4 py-3 leading-10 text-tetriary "
+                {...register('name', { required: true })}
+              />
+              {errors.name && (
+                <span className="text-formAlert">To pole jest wymagane</span>
+              )}
+            </div>
+            <div className="mb-6 flex flex-col">
+              <label
+                htmlFor="email"
+                className="font-normal leading-10 text-tetriary"
+              >
+                E-mail
+              </label>
+              <input
+                type="email"
+                className="form-input mb-3 w-3/5 rounded-2xl border-hidden bg-formField	px-4 py-3 leading-10 text-tetriary "
+                {...register('email', { required: true })}
+              />
+              {errors.email && (
+                <span className="text-formAlert">
+                  Błędny format e-mail (brak znaków specjalnych np. @, .)
+                </span>
+              )}
+            </div>
+            <div className="mb-6 flex flex-col">
+              <label
+                htmlFor="department"
+                className="font-normal leading-10 text-tetriary"
+              >
+                Dział, który Cię interesuje
+              </label>
+              <select
+                placeholder="Wybierz z listy"
+                className="form-input mb-3 w-3/5 rounded-2xl border-hidden bg-formField	px-4 py-3 leading-10 text-tetriary placeholder:text-tetriary"
+                {...register('department')}
+              >
+                <option value="webDevelopment">Web development</option>
+                <option value="projectManagement">Project management</option>
+                <option value="onlineMarketing">Online marketing</option>
+                <option value="uxUiDesign">UX/UI design</option>
+                <option value="HumanResources">Human Resources</option>
+              </select>
+            </div>
+          </div>
+          <div className="mb-5 flex flex-col">
+            <label
+              htmlFor="message"
+              className="font-normal leading-10 text-tetriary"
+            >
+              Wiadomość:
+            </label>
+            <textarea
+              className="form-textarea mb-3 block h-[314px] w-3/5 rounded-2xl border-hidden bg-formField p-4 px-4 py-3 leading-10 text-tetriary placeholder:text-tetriary"
+              {...register('message', { required: true })}
+              placeholder="Parę słów o Tobie i Twoim doświadczeniu"
+            />
+            {errors.message && (
+              <span className="text-formAlert">To pole jest wymagane</span>
+            )}
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              className="form-checkbox mr-5 h-6 w-6 rounded-md text-primary"
+            />
+            <span className="mb-10 text-sm">
+              Akceptuję Politykę Prywatności serwisu coderscrew.pl i wyrażam
+              zgodę na przetwarzanie przez Stowarzyszenie CodersCrew z siedzibą
+              we Wrocławiu, wpisane do rejestru stowarzyszeń Krajowego Rejestru
+              Sądowego pod numerem KRS 0000744745 (dalej: “Stowarzyszenie”), w
+              celach marketingowych, w tym do przesyłania mi informacji o
+              wydarzeniach organizowanych przez Stowarzyszenie, a także w celu
+              ich wykorzystania w materiałach audio, audiowizualnych,
+              fotograficznych lub na jakichkolwiek innych nośnikach, a także w
+              mediach społecznościowych, portalach i na stronach internetowych
+              Stowarzyszenia do celów marketingowych (promocyjnych)
+              Stowarzyszenia.
             </span>
-          )}
-        </div>
-        <div className="mb-6 flex flex-col">
-          <label
-            htmlFor="selectedDepartment"
-            className="font-normal text-tetriary"
-          >
-            Dział, który Cię interesuje
-          </label>
-          <select
-            placeholder="UX/UI Design"
-            className="form-input mb-3 w-3/5 rounded-lg border-hidden bg-formField	px-4 py-3 leading-10 text-tetriary placeholder:text-tetriary"
-            name="selectedDepartment"
-          >
-            <option value="webDevelopment">Web development</option>
-            <option value="projectManagement">Project management</option>
-            <option value="onlineMarketing">Online marketing</option>
-            <option value="uxUiDesign">UX/UI design</option>
-            <option value="HumanResources">Human Resources</option>
-          </select>
-        </div>
-      </div>
-      <div className="mb-5 flex flex-col">
-        <label htmlFor="message" className="font-normal text-tetriary">
-          Wiadomość:
-        </label>
-        <textarea
-          className="form-textarea mb-3 block h-[314px] w-3/5 rounded-lg border-hidden bg-formField p-4 px-4 py-3 leading-10 text-tetriary placeholder:text-tetriary"
-          {...register('message', { required: true })}
-          placeholder="Parę słów o Tobie i Twoim doświadczeniu"
-        />
-        {errors.message && (
-          <span className="text-marketing">To pole jest wymagane</span>
-        )}
-      </div>
-      <div className="flex flex-row">
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              className="form-checkbox mr-5 h-6 w-6 rounded-md text-primary"
+            />
+            <span className="mb-5 text-sm">
+              Oświadczam, że zapoznałam/em się z “Informacją o przetwarzaniu
+              danych osobowych” zawartej w Polityce Prywatności, zawierającą m.
+              in. informacje o szczegółowych celach przetwarzania moich danych
+              osobowych, o podstawie prawnej ich przetwarzania, o danych
+              kontaktowych Administratora, a także o wszelkich przysługujących
+              mi uprawnieniach i warunkach ich realizacji, w tym o prawie:
+              dostępu do moich danych osobowych, żądania ich usunięcia,
+              uzupełnienia, sprostowania, przeniesienia lub ograniczenia ich
+              przetwarzania, wniesienia sprzeciwu i złożenia skargi do Prezesa
+              Urzędu Ochrony Danych Osobowych. Zostałam/em poinformowana/y, że w
+              każdej chwili przysługuje mi prawo do cofnięcia zgody na
+              przetwarzanie danych osobowych, a wycofanie zgody nie wpływa na
+              zgodność z prawem przetwarzania, którego dokonano na podstawie
+              zgody przed jej wycofaniem.
+            </span>
+          </div>
+        </section>
+        <aside className="flex w-1/2 flex-col">
+          <section className="flex flex-col pb-10">
+            <h2 className="pl-10 text-3.5xl font-bold">Masz pytania?</h2>
+            <h2 className="w-4/5 pb-10 pl-10 text-3.5xl font-bold">
+              Skontaktuj się z nami!
+            </h2>
+            <div className="flex flex-row items-center pb-5 pl-15 font-normal">
+              <Koperta />
+              <p className="pl-4 font-normal leading-normal">
+                kontakt@coderscrew.pl
+              </p>
+            </div>
+            <div className="flex flex-row items-center pb-5 pl-15 font-normal">
+              <Pinezka />
+              <p className="w-2/3 pl-4 font-normal leading-normal">
+                ul. Powstańców Śląskich 5, pokój 610, Wrocław
+              </p>
+            </div>
+          </section>
+        </aside>
+      </main>
+      <div className="flex items-start justify-center leading-tight">
         <input
-          type="checkbox"
-          className="form-checkbox mr-6 rounded text-primary"
+          type="submit"
+          className="inline-flex h-11 w-[175px] items-start justify-center rounded border border-quaternary bg-quaternary bg-center px-4 py-[12.50px] leading-tight text-white"
+          value="Wyślij wiadomość"
         />
-        <span className="mb-10 text-sm">
-          Akceptuję Politykę Prywatności serwisu coderscrew.pl i wyrażam zgodę
-          na przetwarzanie przez Stowarzyszenie CodersCrew z siedzibą we
-          Wrocławiu, wpisane do rejestru stowarzyszeń Krajowego Rejestru
-          Sądowego pod numerem KRS 0000744745 (dalej: “Stowarzyszenie”), w
-          celach marketingowych, w tym do przesyłania mi informacji o
-          wydarzeniach organizowanych przez Stowarzyszenie, a także w celu ich
-          wykorzystania w materiałach audio, audiowizualnych, fotograficznych
-          lub na jakichkolwiek innych nośnikach, a także w mediach
-          społecznościowych, portalach i na stronach internetowych
-          Stowarzyszenia do celów marketingowych (promocyjnych) Stowarzyszenia.
-        </span>
       </div>
-      <div className="flex flex-row">
-        <input
-          type="checkbox"
-          className="form-checkbox mr-6 rounded text-primary"
-        />
-        <span className="mb-5 text-sm">
-          Oświadczam, że zapoznałam/em się z “Informacją o przetwarzaniu danych
-          osobowych” zawartej w Polityce Prywatności, zawierającą m. in.
-          informacje o szczegółowych celach przetwarzania moich danych
-          osobowych, o podstawie prawnej ich przetwarzania, o danych
-          kontaktowych Administratora, a także o wszelkich przysługujących mi
-          uprawnieniach i warunkach ich realizacji, w tym o prawie: dostępu do
-          moich danych osobowych, żądania ich usunięcia, uzupełnienia,
-          sprostowania, przeniesienia lub ograniczenia ich przetwarzania,
-          wniesienia sprzeciwu i złożenia skargi do Prezesa Urzędu Ochrony
-          Danych Osobowych. Zostałam/em poinformowana/y, że w każdej chwili
-          przysługuje mi prawo do cofnięcia zgody na przetwarzanie danych
-          osobowych, a wycofanie zgody nie wpływa na zgodność z prawem
-          przetwarzania, którego dokonano na podstawie zgody przed jej
-          wycofaniem.
-        </span>
-      </div>
-      <input
-        type="submit"
-        className="gap-2.5 value:text-white align-center inline-flex h-11 w-[175px] rounded border border-quaternary bg-quaternary bg-center px-4 py-[12.50px]"
-        value="Wyślij wiadomość"
-      />
     </form>
   );
 }
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -2,8 +2,6 @@ import Confetti from 'assets/confetti.svg';
 import Dialog from 'assets/dialog.svg';
 import Eye from 'assets/eye.svg';
 import File from 'assets/file.svg';
-import Koperta from 'assets/Koperta.svg';
-import Pinezka from 'assets/Pinezka.svg';
 import Thic from 'assets/thic.svg';
 import Vector from 'assets/vector.svg';
 import Zespol from 'assets/zespol.svg';
@@ -111,32 +109,14 @@ const Formular = () => (
         </div>
       </div>
     </section>
-    <div className="pb-10 pt-15 text-2xl font-bold">
+    <div className="w-3/5 pb-10 pl-20 pt-15 text-3.5xl font-bold">
       <h2>Cześć!</h2>
       <h2>Cieszymy się, że chcesz zostać członkiem CodersCrew!</h2>
     </div>
-    <div className="flex w-full flex-col md:flex-row">
-      <div className="flex w-2/3 flex-col">
+    <div className="flex w-full flex-col pl-20 md:flex-row">
+      <div className="w-ful flex flex-col">
         <FormJoin />
       </div>
-      <aside className="flex w-1/3 flex-col">
-        <section className="flex flex-col pb-10">
-          <h2 className="text-2xl font-bold">Masz pytania?</h2>
-          <h2 className="pb-10 text-2xl font-bold">Skontaktuj się z nami!</h2>
-          <div className="flex flex-row items-center pb-5 font-normal">
-            <Koperta />
-            <p className="pl-4 font-normal leading-normal">
-              kontakt@coderscrew.pl
-            </p>
-          </div>
-          <div className="flex flex-row items-center pb-5 font-normal">
-            <Pinezka />
-            <p className="pl-4 font-normal leading-normal">
-              ul. Powstańców Śląskich 5, pokój 610, Wrocław
-            </p>
-          </div>
-        </section>
-      </aside>
     </div>
   </SectionWrapper>
 );
