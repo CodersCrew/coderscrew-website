9994096adfb9216a682d0c2a5a71c191728c2403
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-07-12 20:09:32 +0200

    feat: download logos

--- src/components/PressCard/PressCard.tsx
+++ src/components/PressCard/PressCard.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
 import { ComponentProps } from 'react';
 
-import { Props } from '@/assets';
-
 type ImageProps = ComponentProps<typeof Image>;
 type HoverProps = {
   title: string;
@@ -10,11 +8,13 @@ type HoverProps = {
   src: string;
 };
 
-type PressCardProps = ImageProps & HoverProps & Props;
+type PressCardProps = ImageProps & HoverProps;
 
 export const PressCard = ({ title, alt, src }: PressCardProps) => (
   <div className="w-360 h-263 rounded-8 flex flex-col items-center justify-center gap-4 bg-white p-0 pb-8">
-    <Image src={src} alt={alt} className="w-full bg-white" />
+    <a href={src} download>
+      Download FIle
+    </a>
     <p className="w-112 font-lato leading-120 flex items-center justify-center text-base font-semibold text-black">
       {title}
     </p>
