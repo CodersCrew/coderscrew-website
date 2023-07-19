8ab4956a422b26f7e2da27e8f2aa2393937ac75c
Damian Gatkowski <83064936+damgat@users.noreply.github.com>
2023-07-19 14:13:01 +0200

    Update src/pages/press-pack.tsx
    Co-authored-by: Jędrzej Ratajczak <nilphym@gmail.com>

--- src/pages/press-pack.tsx
+++ src/pages/press-pack.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { SectionWrapper } from '@/components';
 import { PressCard } from '@/components/PressCard';
 import { PressColors } from '@/components/PressColors';
