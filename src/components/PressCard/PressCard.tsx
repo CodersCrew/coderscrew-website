d59d1c44e6be7ff38d9be3703eef55cb18cf9bff
Damian Gatkowski <83064936+damgat@users.noreply.github.com>
2023-07-19 13:55:51 +0200

    Update src/components/PressCard/PressCard.tsx
    Co-authored-by: Jędrzej Ratajczak <nilphym@gmail.com>

--- src/components/PressCard/PressCard.tsx
+++ src/components/PressCard/PressCard.tsx
@@ -1,14 +1,5 @@
 import Image from 'next/image';
-import { ComponentProps } from 'react';
-
-type ImageProps = ComponentProps<typeof Image>;
-type HoverProps = {
-  title: string;
-  alt: string;
-  src: string;
-};
-
-type PressCardProps = ImageProps & HoverProps;
+type PressCardProps = { title: string, alt: string, src: string };
 
 export const PressCard = ({ title, alt, src }: PressCardProps) => (
     <div className="w-360 h-263 rounded-8 flex flex-col items-center justify-center gap-4 bg-white p-0 pb-8">
