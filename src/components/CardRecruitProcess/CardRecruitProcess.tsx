304bc3ecd6ccf68ce98c179b605f04e1a4e6d1e4
Damian <damian.gatkowski@proton.me>
2023-07-20 18:30:02 +0200

    style: card recruit text

--- src/components/CardRecruitProcess/CardRecruitProcess.tsx
+++ src/components/CardRecruitProcess/CardRecruitProcess.tsx
@@ -17,9 +17,9 @@ export const CardRecruitProcess = ({
 }: ImageCardProps) => (
   <div className="flex h-[347px] w-[456px] flex-col py-[27px]">
     <Image {...props} alt={alt} />
-    <p className="text-2xl font-semibold leading-tight text-primary lg:text-3.5xl">
+    <p className="pb-4 text-2xl font-semibold leading-tight text-primary lg:text-3.5xl">
       {title}
     </p>
-    <p className="text-base leading-normal text-primary">{text}</p>
+    <p className="pl-8 text-xl leading-7 text-primary">{text}</p>
   </div>
 );
