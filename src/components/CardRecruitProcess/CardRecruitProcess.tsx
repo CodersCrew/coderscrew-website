a450aa5901e293d120c196c66f305c2b14725db2
Damian <damian.gatkowski@proton.me>
2023-07-21 14:36:07 +0200

    style: image join us fix

--- src/components/CardRecruitProcess/CardRecruitProcess.tsx
+++ src/components/CardRecruitProcess/CardRecruitProcess.tsx
@@ -17,13 +17,7 @@ export const CardRecruitProcess = ({
   alt
 }: ImageCardProps) => (
   <div className="flex h-[347px] w-[456px] flex-col py-[27px]">
-    <Image
-      src={src}
-      alt={alt}
-      width="392"
-      height="192"
-      className="w-full bg-white"
-    />
+    <Image src={src} alt={alt} width="392" height="192" className="w-full" />
     <p className="pb-4 text-2xl font-semibold leading-tight text-primary lg:text-3.5xl">
       {title}
     </p>
