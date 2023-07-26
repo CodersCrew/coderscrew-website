e834de9ed2e18b1c6959815161c1176e6771ed85
kashubian <karalusmichal@gmail.com>
2023-07-26 12:57:24 +0200

    fix: redirecting to projects and center HowWeWork button

--- src/containers/home/HowWeWork.tsx
+++ src/containers/home/HowWeWork.tsx
@@ -3,11 +3,11 @@ import { Card, Link } from '@/components';
 
 export const HowWeWork = () => (
   <div className="mx-auto flex max-w-screen-2xl flex-col items-center gap-8 px-4 py-24 lg:items-stretch lg:gap-34 lg:px-30">
-    <h2 className="mt-6 self-center pl-8 text-2xl font-semibold uppercase text-quaternary lg:-mb-20 lg:pl-0 lg:font-bold">
-      Jak działamy?
-    </h2>
     <div className="grid items-end justify-center gap-6 lg:grid-cols-[50%_50%] lg:gap-0">
       <div className="flex flex-col items-start bg-additional-white lg:gap-8 lg:bg-additional-darkWhite">
+        <h2 className="mb-4 mt-6 pl-8 text-2xl font-semibold uppercase text-quaternary lg:pl-0 lg:font-bold">
+          Jak działamy?
+        </h2>
         <Card
           header=""
           title="Online"
@@ -21,8 +21,10 @@ export const HowWeWork = () => (
       </div>
       <UndrawWorld className="px-7" />
     </div>
-    <Link href="/faq" variant="filled" className="self-center">
-      Zobacz więcej
-    </Link>
+    <div className="self-center">
+      <Link href="/faq" variant="filled">
+        Zobacz więcej
+      </Link>
+    </div>
   </div>
 );
