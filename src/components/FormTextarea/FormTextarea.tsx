fa2f19997497d82eeb8d43ca7a0419be1571bd05
Michał Gawroński <mic.gawronski@gmail.com>
2023-09-26 13:21:36 +0200

    feat: add red cross UI to FormTextarea

--- src/components/FormTextarea/FormTextarea.tsx
+++ src/components/FormTextarea/FormTextarea.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FieldErrors, UseFormRegister } from 'react-hook-form';
 
+import RedCross from '@/assets/red-cross-form-error.svg';
+
 import { Inputs } from '../FormJoin/FormJoin';
 
 type InputProps = {
@@ -23,11 +25,14 @@ export function FormTextarea({
       <label htmlFor={htmlFor} className="font-normal leading-10 text-tetriary">
         {content}
       </label>
-      <textarea
-        className="form-textarea mb-3 block h-[314px] rounded-2xl border-hidden bg-formField p-4 px-4 py-3 leading-10 text-tetriary placeholder:text-tetriary md:w-4/6"
-        {...register('message', { required: 'To pole jest wymagane' })}
-        placeholder={placeholder}
-      />
+      <div className="flex flex-row items-center">
+        <textarea
+          className="form-textarea mb-3 mr-1 block h-[314px] rounded-2xl border-hidden bg-formField p-4 px-4 py-3 leading-10 text-tetriary placeholder:text-tetriary md:w-4/6"
+          {...register('message', { required: 'To pole jest wymagane' })}
+          placeholder={placeholder}
+        />
+        {errors.message && <RedCross />}
+      </div>
       {errors.message && (
         <span className="text-formAlert">
           {errors[htmlFor]?.message?.toString()}
